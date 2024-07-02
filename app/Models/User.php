7f1094ab7d601fd0fcafddf63c8e9846ef34c02e
pearkie <pearkieveern30@gmail.com>
2024-07-02 21:36:03 +0800

    remove

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,17 +31,4 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
