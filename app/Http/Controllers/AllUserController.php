be7731822802e34a133254d8b073b9a36600ceb6
evrylsem <evrylclaire.sembrino.21@usjr.edu.ph>
2024-07-04 08:40:44 +0800

    one users table

--- app/Http/Controllers/AllUserController.php
+++ app/Http/Controllers/AllUserController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Company;
-use App\Http\Requests\StoreCompanyRequest;
-use App\Http\Requests\UpdateCompanyRequest;
+use App\Models\AllUser;
+use App\Http\Requests\StoreAllUserRequest;
+use App\Http\Requests\UpdateAllUserRequest;
 
-class CompanyController extends Controller
+class AllUserController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -27,7 +27,7 @@ class CompanyController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCompanyRequest $request)
+    public function store(StoreAllUserRequest $request)
     {
         //
     }
@@ -35,7 +35,7 @@ class CompanyController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Company $company)
+    public function show(AllUser $allUser)
     {
         //
     }
@@ -43,7 +43,7 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Company $company)
+    public function edit(AllUser $allUser)
     {
         //
     }
@@ -51,7 +51,7 @@ class CompanyController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCompanyRequest $request, Company $company)
+    public function update(UpdateAllUserRequest $request, AllUser $allUser)
     {
         //
     }
@@ -59,7 +59,7 @@ class CompanyController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Company $company)
+    public function destroy(AllUser $allUser)
     {
         //
     }
